52baa322415954b7c69e24dbf4aba228187ad099
Rohandede1825 <rohandede97@gmail.com>
2025-01-13 16:03:23 +0530

    error-1

--- app/Login/page.js
+++ app/Login/page.js
@@ -52,20 +52,20 @@ const page = () => {
 
 
 
-  const notify = (e)=>{
-    e.preventDefault();
-  toast.success('Login Successful!', {
-    position: "top-right",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: false,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    onClick: ()=>console.log('clicked'),
-    theme: "light",
-    });
-  };
+  // const notify = (e)=>{
+  //   e.preventDefault();
+  // toast.success('Login Successful!', {
+  //   position: "top-right",
+  //   autoClose: 5000,
+  //   hideProgressBar: false,
+  //   closeOnClick: false,
+  //   pauseOnHover: true,
+  //   draggable: true,
+  //   progress: undefined,
+  //   onClick: ()=>console.log('clicked'),
+  //   theme: "light",
+  //   });
+  // };
 
 
   return (
@@ -87,7 +87,7 @@ const page = () => {
           className='border-2 border-red-500 rounded-lg p-2 focus:border-red-600'
           onChange={handleChange}
         />
-        <button onClick={notify} disabled={loading} className=' bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg disabled:opacity-90'>
+        <button  disabled={loading} className=' bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg disabled:opacity-90'>
         {loading ? 'Loading...' : 'Login'}
         </button>
       </form>
--- app/signup/page.js
+++ app/signup/page.js
@@ -9,6 +9,7 @@ const page = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const router = useRouter();
+
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -46,26 +47,26 @@ const page = () => {
     }
   };
 
-  const notify = (e)=>{
-    e.preventDefault()
-    toast.success('Account Created Successfully!', {
-      position: "top-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: false,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "light",
+  // const notify = (e)=>{
+  //   e.preventDefault()
+  //   toast.success('Account Created Successfully!', {
+  //     position: "top-right",
+  //     autoClose: 5000,
+  //     hideProgressBar: false,
+  //     closeOnClick: false,
+  //     pauseOnHover: true,
+  //     draggable: true,
+  //     progress: undefined,
+  //     theme: "light",
 
-      });
+  //     });
 
-  }
+  // }
 
-  const handleGoogleLogin= (e)=>{
-    e.preventDefault()
-    console.log("handle Google login successfully")
-  }
+  // const handleGoogleLogin= (e)=>{
+  //   e.preventDefault()
+  //   console.log("handle Google login successfully")
+  // }
 
   return (
     <div onSubmit={handleSubmit} className='p-3 max-w-lg mx-auto mt-8 bg-red-100  shadow-xl '>
@@ -92,15 +93,15 @@ const page = () => {
           className='border-2 border-red-500 rounded-lg p-2 focus:border-red-600'
           onChange={handleChange}
         />
-        <button  disabled={loading}onClick={notify} className=' bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg disabled:opacity-90'>
+        <button  disabled={loading} className=' bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg disabled:opacity-90'>
         {loading ? 'Loading...' : 'Sign Up'}
         </button>
 
        
-  <button onClick={handleGoogleLogin} className='flex items-center gap-2 bg-white text-black hover:bg-gray-100 font-bold py-2 px-4 rounded-lg disabled:opacity-90 justify-center flex-1'>
+  {/* <button className='flex items-center gap-2 bg-white text-black hover:bg-gray-100 font-bold py-2 px-4 rounded-lg disabled:opacity-90 justify-center flex-1'>
     <svg className='w-7 h-auto' width="800px" height="800px" viewBox="-3 0 262 262" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"><path d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027" fill="#4285F4"/><path d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1" fill="#34A853"/><path d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782" fill="#FBBC05"/><path d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251" fill="#EB4335"/></svg>
     <span className=''>Signup with Google</span>
-  </button>
+  </button> */}
 
 
       </form>

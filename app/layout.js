efcc616830e9bf0ab1d4d2128da60acc4d7ec43b
Rushikesh <rishisurya1320@gmail.com>
2025-01-14 12:44:49 +0530

    layout

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,42 @@
-"use client";
+"use client"
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import { ToastContainer } from 'react-toastify';
 import Navbar from "./components/header";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+
 
 export default function RootLayout({ children }) {
   return (
-    <Provider store={store}>
-      <Navbar />
-      {children}
-      <ToastContainer />
-    </Provider>
+    
+    <html lang="en">
+     
+
+
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Provider store={store}>
+         <Navbar/>
+        {children}
+        <ToastContainer />
+        </Provider>
+      </body>
+    </html>
+    
   );
 }

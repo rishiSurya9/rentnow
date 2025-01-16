f1e71da7df7deb4751464fdd5f88f6cbfb66b8f3
Rohandede1825 <rohandede97@gmail.com>
2025-01-16 13:30:06 +0530

    Authentication 4

--- app/components/header.js
+++ app/components/header.js
@@ -66,8 +66,20 @@ const Header = () => {
               </li>
             </Link>
             <Link href="/Profile">
+            {currentUser ?(
+             <img
+             
+             src= {currentUser.avatar}
+             alt="profile"
+             className="rounded-full mx-auto h-7 w-7 object-cover cursor-pointer self-center "
+           />
+            ):(
               <li className="md:text-white hover:text-lg  hover:underline inline-block transition-all duration-300">
-                Profile
+                  Profile
+                </li>
+            )}
+              <li className="md:text-white hover:text-lg  hover:underline inline-block transition-all duration-300">
+                
               </li>
             </Link>
 
--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,19 @@
-"use client"
+"use client";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import dotenv from 'dotenv';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store';
+import dotenv from "dotenv";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
-// import profile from "./Profile/page";
-// import { Route } from "react-router-dom";
-// import Router from "next/router";
+import profile from "./Profile/page";
+import { Route, Routes } from "react-router-dom";
+import Router from "next/router";
 
-// import privateRoute from "./components/privateRoute";
+import privateRoute from "./components/privateRoute";
 
 import Navbar from "./components/header";
-
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,37 +25,23 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-
-
 export default function RootLayout({ children }) {
-  
   dotenv.config();
 
-  
   return (
-    
     <html lang="en">
-     
-
-
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Provider store={store}>
-          
           <PersistGate loading={null} persistor={persistor}>
-         <Navbar/>
-        {children}
-        <ToastContainer />
-
-       
-
-      
-
-        </PersistGate>
+            <Navbar />
+            {children}
+            <ToastContainer />
+           
+          </PersistGate>
         </Provider>
       </body>
     </html>
-    
   );
 }

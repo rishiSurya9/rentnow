028413329c94da1fbbd883bdc61edca10e837bf6
Rohandede1825 <rohandede97@gmail.com>
2025-01-25 23:49:50 +0530

    Add clipboard-copy import to listing page component

--- app/Listing/[id]/page.js
+++ app/Listing/[id]/page.js
@@ -4,6 +4,7 @@ import {Swiper, SwiperSlide} from 'swiper/react';
 import SwiperCore from 'swiper';
 import {Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
+import copied from 'clipboard-copy';
 import {
   FaBath,
   FaBed,

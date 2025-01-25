ac32f840e73076d7a0655ba076f7139c716728b0
Rushikesh <rishisurya1320@gmail.com>
2025-01-25 21:23:51 +0530

    listing 1

--- app/Listing/[id]/page.js
+++ app/Listing/[id]/page.js
@@ -1,24 +1,39 @@
 "use client";
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const page = ({params}) => {
     const { id } = params;
+    
+   const [listing, setListing] = React.useState(null);
+   const [loading, setLoading] = React.useState(true);
+   const [error, setError] = React.useState(false);
    useEffect(() => {
-    const fetchListing = async () => {
-      const listingId = id;
-      const res = await fetch(`${process.env.PUBLIC_API}/api/listing/get/${listingId}`);
-      const data = await res.json();
-      if (res.ok) {
-       console.log(data);
-      } else {
-        console.log(data.message);
-      }
-    };
-    fetchListing();
-  }, [id]);
+     const fetchListing = async () => {
+        try{
+            setLoading(true);
+       const res = await fetch(`${process.env.NEXT_PUBLIC_API}/api/listing/get/${id}`);
+       const data = await res.json();
+       if (res.ok) {
+         setListing(data);
+         setLoading(false);
+       } else {
+         setError(true);
+         setLoading(false);
+       }
+    } catch (error) {
+      setError(true);
+      setLoading(false);
+    }
+      
+     };
+     fetchListing();
+   }, [id]);
+ 
   return (
     <div>
-      <h2>Listing {id}</h2>
+      {listing && <h1>{listing.name}</h1>}
+      {error && <h1>Error fetching listing</h1>}
+      {loading && <h1>Loading...</h1>}
     </div>
   )
 }

7baff2da046d0b1ebbd9e65ccb8c260259bdae52
Rohandede1825 <rohandede97@gmail.com>
2025-01-25 23:58:55 +0530

    Remove unused imports from listing page component

--- app/Listing/[id]/page.js
+++ app/Listing/[id]/page.js
@@ -4,15 +4,7 @@ import {Swiper, SwiperSlide} from 'swiper/react';
 import SwiperCore from 'swiper';
 import {Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
-import copied from 'clipboard-copy';
-import {
-  FaBath,
-  FaBed,
-  FaChair,
-  FaMapMarkerAlt,
-  FaParking,
-  FaShare,
-} from 'react-icons/fa';
+
 
 const page = ({params}) => {
     SwiperCore.use([Navigation]);

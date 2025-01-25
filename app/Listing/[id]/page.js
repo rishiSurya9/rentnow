ae675b9350cdd3ebace66268d067f56ff71b9efa
Rushikesh <rishisurya1320@gmail.com>
2025-01-25 23:03:25 +0530

    sliding 4

--- app/Listing/[id]/page.js
+++ app/Listing/[id]/page.js
@@ -15,7 +15,7 @@ const page = ({params}) => {
      const fetchListing = async () => {
         try{
             setLoading(true);
-       const res = await fetch(`${process.env.NEXT_PUBLIC_API}/api/listing/get/${id}`);
+       const res = await fetch(`${process.env.PUBLIC_API}/api/listing/get/${id}`);
        const data = await res.json();
        if (res.ok) {
          setListing(data);

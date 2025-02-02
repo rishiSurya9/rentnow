17566dcd7a10974e56eeb3b0563ddd8d3460a735
Rohandede1825 <rohandede97@gmail.com>
2025-02-02 22:50:33 +0530

    Enhance ListingItem component with additional details and integrate line-clamp for description truncation

--- app/components/ListingItem.js
+++ app/components/ListingItem.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 import Link from 'next/link';
 import { MdLocationOn } from 'react-icons/md';
+import { list } from 'postcss';
 
 const ListingItem = ({ listing }) => {
   return (
@@ -23,9 +24,24 @@ const ListingItem = ({ listing }) => {
 
             <div className="flex items-center gap-1 ">
               <MdLocationOn className="h-4 w-4 text-green-700" />
-              <p className="text-gray-600 text-sm truncate ">
+              <p className="text-gray-600 text-sm truncate w-full ">
                 {listing.address}
               </p>
+            </div>
+               <p className='text-sm text-gray-600 line-clamp-2'>{listing.description}</p>
+
+               <p className='text-slate-500 mt-2 font-semibold flex items-center'>$
+                  {listing.offer?listing.discountPrice.toLocalString('en-US'):regularPrice.toLocalString('en-US')}
+                  {listing.type === 'rent' && '/month'}
+               </p>
+            
+            <div className='text-slate-700 flex gap-4'>
+               <div className='font-bold text-xs text-gray-600'>
+                  {listing.bedrooms>1 ? `${listing.bedrooms} beds` : `${listing.bedrooms} bed`}
+               </div>
+               <div className='font-bold text-xs text-gray-600'>
+                  {listing.bathrooms>1 ? `${listing.bathrooms} baths` : `${listing.bathrooms} bath`}
+               </div>
             </div>
           </div>
         </Link>

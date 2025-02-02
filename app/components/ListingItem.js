4c3320cd4b7194c93c1d69ce99955f45e0957f1c
Rohandede1825 <rohandede97@gmail.com>
2025-02-02 18:45:16 +0530

    Enhance ListingItem component to display listing image and name multiple times

--- app/components/ListingItem.js
+++ app/components/ListingItem.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const ListingItem = ({listing}) => {
   return (
+    <>
     <div>{listing.name}</div>
+    <div>{listing.img}</div>
+    <div>{listing.name}</div>
+    <div>{listing.name}</div>
+
+    </>
   )
 }
 

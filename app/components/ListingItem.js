6fc1cfb5a0157d1991e052ff2048043746b6d76d
Rohandede1825 <rohandede97@gmail.com>
2025-02-02 19:21:38 +0530

    Add alt text and class to listing image in ListingItem component

--- app/components/ListingItem.js
+++ app/components/ListingItem.js
@@ -5,7 +5,9 @@ const ListingItem = ({listing}) => {
   return (
     <>
         <Link to={`/listing/${listing._id}`}>   
-              <img src={listing.imageUrls[0]} alt="" />
+              <img src={listing.imageUrls[0]} alt="listing cover"
+              className='h-'
+              />
         </Link>
 
     </>

f88cacd63ab583c9f8ef66058512d14c282f0204
Rohandede1825 <rohandede97@gmail.com>
2025-02-03 21:59:50 +0530

    Update API calls in content.js to use PUBLIC_API environment variable

--- app/components/content.js
+++ app/components/content.js
@@ -16,7 +16,7 @@ export default function Home() {
   useEffect(() => {
     const fetchOfferListings = async () => {
       try {
-        const res = await fetch('/api/listing/get?offer=true&limit=4');
+        const res = await fetch(`${process.env.PUBLIC_API}/api/listing/get?offer=true&limit=4`);
         const data = await res.json();
         setOfferListings(data);
         fetchRentListings();
@@ -26,7 +26,7 @@ export default function Home() {
     };
     const fetchRentListings = async () => {
       try {
-        const res = await fetch('/api/listing/get?type=rent&limit=4');
+        const res = await fetch(`${process.env.PUBLIC_API}/api/listing/get?type=rent&limit=4`);
         const data = await res.json();
         setRentListings(data);
         fetchSaleListings();
@@ -37,7 +37,7 @@ export default function Home() {
 
     const fetchSaleListings = async () => {
       try {
-        const res = await fetch('/api/listing/get?type=sale&limit=4');
+        const res = await fetch(`${process.env.PUBLIC_API}/api/listing/get?type=sale&limit=4`);
         const data = await res.json();
         setSaleListings(data);
       } catch (error) {
@@ -133,3 +133,184 @@ export default function Home() {
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 'use client'
+// import React, { useState, useEffect } from 'react';
+// import Link from 'next/link';
+// import SwiperCore from 'swiper';
+// import { Navigation } from 'swiper/modules';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css/bundle';
+// import 'swiper/css/navigation';
+// import ListingItem from './ListingItem';
+
+
+
+// const content = () => {
+//     const [offerLisiting, setofferLisiting] = useState([]);
+//     const [saleListing, setsaleListing] = useState([]);
+//     const [rentListing, setRentListing] = useState([]);
+//     SwiperCore.use(Navigation)
+
+//     useEffect (()=>{
+//         const fetchOfferLisitings = async () => {
+//             try {
+//              const res = await fetch(`${process.env.PUBLIC_API}/api/listing/get?offer=true&limit=4`);
+//              const data = await res.json();
+//              setofferLisiting(data);
+//             } catch (error) {
+//                 console.log(error);
+                
+//             }
+//         }
+        
+//         const fetchRentListings = async () => {
+//             try {
+//              const res = await fetch(`${process.env.PUBLIC_API}/api/listing/get?type=rent&limit=4`);
+//              const data = await res.json();
+//              setsaleListing(data);
+//             } catch (error) {
+//                 console.log(error);
+//             }
+//         }
+
+//         const fetchSaleListings = async () => {
+//             try {
+//              const res = await fetch(`${process.env.PUBLIC_API}/api/listing/get?type=sale&limit=4`);
+//              const data = await res.json();
+//              setRentListing(data);
+//             } catch (error) {
+//                 console.log(error);
+//             }
+//         }
+
+//         fetchSaleListings();
+//         fetchRentListings();
+//         fetchOfferLisitings();
+//     },[])
+
+
+//   return (
+//     <>
+//       {/* top */}
+//       <div className='flex flex-col gap-6  p-28 px-3 max-w-6xl mx-auto'>
+//         <h1 className=' text-slate-700 font-bold text-3xl lg:text-6xl '>
+//             Find Your next <span className='text-slate-500'>perfect</span>
+//             <br />
+//         </h1>
+
+//         <div className='text-gray-400 tet-xs sm: text-sm '>
+//             Rentnow is best place to find you next perfect rent house 
+//             place to live
+//             <br />
+//             We have a wide range of properties for you to 
+//             choose form.
+//         </div>
+
+//         <Link href={'/Search'} className='text-xs sm:text-sm text-blue-800 font-bold hover:underline '>
+//              Lets get started....
+//         </Link>
+//       </div>
+
+//       {/* swipper */}
+        
+//         <Swiper Navigation>
+//             {
+//                 offerLisiting && offerLisiting.length > 0 && 
+//                 offerLisiting.map ((listing)=>(
+//                     <SwiperSlide>
+//                         <div style={{background:`url(${listing.imageUrls[0]} )  center no-repeat`, backgroundSize:'cover'}}  className='h-[500px]' key= {listing._id}>
+
+//                         </div>
+//                     </SwiperSlide>
+//                 ))
+//             }
+//         </Swiper>
+
+
+
+//       {/* lisiting  */}
+//       <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
+//             {
+//                 offerLisiting && offerLisiting.length > 0 &&(
+//                     <div>
+//                         <div>
+//                             <h2 className='text-2xl font-semibold text-slate-600'> Recent Offers</h2>
+//                             <Link className='text-sm text-blue-800 hover:underline' href={'/Search?offer=true'}>
+//                                 Show more offers
+//                             </Link>
+//                         </div>
+
+//                         <div className='flex flex-wrap gap-4'>
+//                             {
+//                                 offerLisiting.map((listing)=>(
+//                                     <ListingItem listing={listing} key={listing._id} />
+//                                 ))
+//                             }
+//                         </div>
+//                     </div>
+//                 )
+//             }
+//       </div>
+//     </>
+//   );
+// };
+
+// export default content;

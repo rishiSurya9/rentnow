957111238a902932917d386c62df67cb61ce28d6
Rohandede1825 <rohandede97@gmail.com>
2025-03-18 12:13:40 +0530

    test 10

--- app/components/ListingItem.js
+++ app/components/ListingItem.js
@@ -29,8 +29,8 @@ const ListingItem = ({ listing }) => {
               {listing.description}
             </p>
 
-            <p className="text-slate-500 mt-2 font-semibold flex items-center">
-              $
+            <p className="text-slate-500  font-semibold flex items-center">
+              rs
               {listing.offer
                 ? listing.discountPrice.toLocaleString('en-US')
                 : listing.regularPrice.toLocaleString('en-US')}
